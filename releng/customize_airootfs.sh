feaac98fb1939e101137bb0021fe7ba88d46b9c3
Sepahrad ghorbani <ghorbanisepahrad1@gmail.com>
2026-02-01 23:29:23 +0330

    Enable sddm service in customize_airootfs.sh

--- releng/customize_airootfs.sh
+++ releng/customize_airootfs.sh
@@ -2,4 +2,4 @@ pacman-key --init
 pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key 3056513887B78AEB
 pacman -Sy --noconfirm chaotic-keyring chaotic-mirrorlist
-
+systemctl enable sddm
