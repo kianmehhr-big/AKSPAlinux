556e71959dcc3aab59cc8326f5fe0ab9468f269e
p0uya3767 <pouyaizadi1389@gmail.com>
2026-02-09 08:25:24 +0330

    fixing sddm running too soon

--- releng/customize_airootfs.sh
+++ releng/customize_airootfs.sh
@@ -1,5 +1,7 @@
 pacman-key --init
 pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key 3056513887B78AEB
-pacman -Sy --noconfirm chaotic-keyring chaotic-mirrorlist
+# pacman -Sy --noconfirm chaotic-keyring chaotic-mirrorlist
 systemctl enable sddm
+systemctl stop sddm
+# systemctl start sddm
